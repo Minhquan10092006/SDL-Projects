50506116226968fe0ff06dedacb4e8bc6b459139
Nguyễn Minh Quân <24022825@vnu.edu.vn>
2025-02-28 16:40:21 +0700

    test thu mixer va load image

--- SDL Projects/main.cpp
+++ SDL Projects/main.cpp
@@ -1,32 +1,108 @@
 #include <SDL.h>
+#include <SDL_image.h>
 #include <SDL_mixer.h>
 #include <iostream>
 
-int main(int argc, char* argv[]) {
-    if (SDL_Init(SDL_INIT_AUDIO) < 0) {
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+
+bool init(SDL_Window*& window, SDL_Renderer*& renderer) {
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0) {
         std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
-        return 1;
+        return false;
+    }
+
+    window = SDL_CreateWindow("SDL Image & MP3 Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (!window) {
+        std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        return false;
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        std::cerr << "Renderer could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        return false;
+    }
+
+    if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
+        std::cerr << "SDL_image could not initialize! IMG_Error: " << IMG_GetError() << std::endl;
+        return false;
     }
 
     if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
         std::cerr << "SDL_mixer could not initialize! Mix_Error: " << Mix_GetError() << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+SDL_Texture* loadTexture(const std::string& path, SDL_Renderer* renderer) {
+    SDL_Texture* newTexture = nullptr;
+    SDL_Surface* loadedSurface = IMG_Load(path.c_str());
+
+    if (!loadedSurface) {
+        std::cerr << "Failed to load image! IMG_Error: " << IMG_GetError() << std::endl;
+    }
+    else {
+        newTexture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
+        SDL_FreeSurface(loadedSurface);
+    }
+
+    return newTexture;
+}
+
+void close(SDL_Window*& window, SDL_Renderer*& renderer, SDL_Texture*& texture, Mix_Music*& music) {
+    SDL_DestroyTexture(texture);
+    Mix_FreeMusic(music);
+    Mix_CloseAudio();
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    IMG_Quit();
+    SDL_Quit();
+}
+
+int main(int argc, char* argv[]) {
+    SDL_Window* window = nullptr;
+    SDL_Renderer* renderer = nullptr;
+
+    if (!init(window, renderer)) {
+        return 1;
+    }
+
+    SDL_Texture* imageTexture = loadTexture("map.png", renderer);
+    if (!imageTexture) {
+        Mix_Music* dummyMusic = nullptr; // Declare a dummy Mix_Music* variable
+        close(window, renderer, imageTexture, dummyMusic);
         return 1;
     }
 
     Mix_Music* bgm = Mix_LoadMUS("background.mp3");
     if (!bgm) {
         std::cerr << "Failed to load music! Mix_Error: " << Mix_GetError() << std::endl;
+        close(window, renderer, imageTexture, bgm);
         return 1;
     }
 
     Mix_PlayMusic(bgm, -1);
     std::cout << "Playing music... Press Enter to quit." << std::endl;
-    std::cin.get();
 
-    Mix_FreeMusic(bgm);
-    Mix_CloseAudio();
-    SDL_Quit();
+    bool quit = false;
+    SDL_Event e;
 
-    
+    while (!quit) {
+        while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, imageTexture, NULL, NULL);
+        SDL_RenderPresent(renderer);
+    }
+
+    close(window, renderer, imageTexture, bgm);
+    return 0;
 }
 
